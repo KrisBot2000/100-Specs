6445770c4132a46b5c25450084369a30c24088dd
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-07-12 09:02:35 -1000

    step 54

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1007,7 +1007,11 @@ class Box {
  * Door class
  * @param {boolean} isOpen Whether the door is opened or closed
  */
-
+class Door {
+  constructor(isOpen){
+    this.isOpen = isOpen;
+  }
+}
 
 /**
  * Step 55
