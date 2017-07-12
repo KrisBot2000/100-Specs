0126097429bcd1123d69b66accf93dde4e5d7b26
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-07-11 20:14:34 -1000

    step 31

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -544,7 +544,22 @@ Pen.prototype.write = function(message){
  *   grow
  *
  */
+function Garden(plantsTotal){
+  this.plantsTotal = plantsTotal;
+  this.isWatered = false;
+}
 
+Garden.prototype.water = function(){
+  this.isWatered = true;
+}
+Garden.prototype.grow = function(){
+  if(this.isWatered ===true){
+    this.plantsTotal += 1;
+    this.isWatered = false;
+  }else{
+    return false;
+  }
+}
 
 /* Step 32
  *
